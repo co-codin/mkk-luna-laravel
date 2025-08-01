46ca55aa5e63c8c345f81f04b0f08c66ffa8770d
elijah <cuiyeqing960904@gmail.com>
2025-08-01 16:20:27 +0300

    middleware

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     protected $guarded = ['id'];
+
+    public static function getDescendantIds($parentId, $depth = 0, $maxDepth = 3)
+    {
+        if ($depth >= $maxDepth) {
+            return [];
+        }
+
+        $ids = [$parentId];
+        $children = self::query()->where('parent_id', $parentId)->pluck('id');
+
+        foreach ($children as $childId) {
+            $ids = array_merge($ids, self::getDescendantIds($childId, $depth + 1));
+        }
+
+        return $ids;
+    }
 }
